8176618c0d6c959064942a10ec26be746f9e865e
Jicheol Oh <jicheol@gmail.com>
2019-06-18 15:08:46 -0700

    기본 라우트 준비하기

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// 이 컴포넌트는 우리의 웹어플리케이션에 BrowserRouter를 적용합니다. 나중에 리덕스를 적용 하게 될 때, 여기서 Provider 를 통하여 프로젝트에 리덕스를 연결시켜줍니다.
 import Root from './client/Root';
 //import registerServiceWorker from './registerServiceWorker';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
+
+
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 //registerServiceWorker();
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+// 이 컴포넌트를 불러와서 한 파일로 내보낼 수 있도록 인덱스를 만들어주세요.
+export { default as Home } from './Home';
+export { default as About } from './About';
