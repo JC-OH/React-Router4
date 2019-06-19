c612352e44d52bd279f8485d709922e3e742453e
Jicheol Oh <jicheol@gmail.com>
2019-06-19 08:54:45 -0700

    HoC 를 사용하여 더 쉽게 코드 스플리팅하기 - 리액트 라우터와 함께 사용하기

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 // 이 컴포넌트를 불러와서 한 파일로 내보낼 수 있도록 인덱스를 만들어주세요.
-export { default as Home } from './Home';
-export { default as About } from './About';
-export { default as Posts } from './Posts';
-export { default as Post } from './Post';
+
+import withSplitting from 'lib/withSplitting';
+
+
+// export { default as Home } from './Home';
+// export { default as About } from './About';
+// export { default as Posts } from './Posts';
+// export { default as Post } from './Post';
+
+export const Home = withSplitting(() => import('./Home'));
+export const About = withSplitting(() => import('./About'));
+export const Posts = withSplitting(() => import('./Posts'));
+export const Post = withSplitting(() => import('./Post'));
